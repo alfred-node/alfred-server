e28f58dad044025cbb24c4a51556e84421d50c4e
Luke Briggs <luke@kulestar.com>
2018-01-15 00:45:07 +0000

    When installed globally Alfred will run as a service after being started at least once

--- bin/alfred-ci.js
+++ bin/alfred-ci.js
@@ -1,5 +1,25 @@
 #!/usr/bin/env node
 
+var service = require ("os-service");
+var process = require('process');
+var path = require('path');
+
+// The working dir where alfred was invoked:
+var calledFromPath = process.cwd();
+
+// Change to the Alfred dir:
+process.chdir(path.dirname(__filename) + '/../');
+
 require('../source/start.js')({
-	loadCommandLine: true
+	loadCommandLine: true,
+	calledFromPath
+});
+
+// Also make sure auto start is setup:
+service.add ("alfred-ci", {
+	programPath: "alfred-ci"
+}, function(error){
+	if(error){
+		console.log('Unable to add alfred as a service', error);
+	}
 });
