455339aad2fe1a8c61d45ed98f92c4254eb5ef9c
Luke Briggs <luke@kulestar.com>
2018-06-04 00:58:00 +0100

    Improved handling for webhooks

--- source/endpoints/v1/pipeline/start.js
+++ source/endpoints/v1/pipeline/start.js
@@ -5,10 +5,23 @@ module.exports = app => (request, response) => {
 		id
 	} = request.query;
 	
+	var config = {
+		workspace:{
+			request
+		}
+	};
+	
+	// Try and find a special webhook handler:
+	var hookHandler = app.webhooks && app.webhooks.find(hook => hook.detect && hook.detect(request));
+	
+	if(hookHandler && hookHandler.onRequest){
+		// Give this hook handler the config object and also the 
+		// original request so it can configure the run accordingly.
+		hookHandler.onRequest(request, config);
+	}
+	
 	// Instantly run it (TODO: Internally should check if it's already running):
-	app.pipeline.run(id, {
-		webhook: request.body,
-	}, {
+	app.pipeline.run(id, config, {
 		onStart: pipe => {
 			
 			// Build started - we won't wait for it though!
