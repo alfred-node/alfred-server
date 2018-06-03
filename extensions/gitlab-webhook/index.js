7919b41fbe93aec3f9588a3df9a9a0701d228a8b
Luke Briggs <luke@kulestar.com>
2018-06-04 00:57:23 +0100

    Gitlab webhooks can now define the branch

--- extensions/gitlab-webhook/index.js
+++ extensions/gitlab-webhook/index.js
@@ -4,12 +4,30 @@ module.exports = app => {
 	app.webhooks.push(
 		{
 			// Is the given request a Gitlab webhook?
-			detect: request => !!request.headers['X-Gitlab-Event'],
+			detect: request => {
+				return !!request.headers['x-gitlab-event']
+			},
 			
 			// Maps a Gitlab request to the Alfred API format.
-			map: request => request.body
+			onRequest: (request, config) => {
+				
+				var glEvent = request.headers['x-gitlab-event'];
+				
+				if(glEvent == 'Push Hook' || glEvent =='Tag Push Hook'){
+					
+					// Primary thing is to establish which branch is being pushed:
+					var branchParts = request.body.ref ? request.body.ref.split('/') : null;
+					var branch = branchParts && branchParts.length ? branchParts[branchParts.length-1] : null;
+					
+					if(branch){
+						// Apply branch:
+						config.workspace.branch = branch;
+					}
+					
+				}
+				
+			}
 		}
 	);
 	
 };
-	
