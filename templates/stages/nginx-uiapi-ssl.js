4ba7de91ce23987be38d0d0dcf68b0b0aa74e1e6
Luke Briggs <luke@kulestar.com>
2018-06-04 00:19:58 +0100

    A sample stage which configures NGINX on a remote host with a PHP API

---
+++ templates/stages/nginx-uiapi-ssl.js
@@ -0,0 +1,19 @@
+/*
+ NGINX UI/API (With SSL support via Let's Encrypt).
+ Sets up NGINX config for a UI/API pair, with a PHP API.
+ 
+ Config:
+ 
+ {
+	domain: 'hello.world.com'
+ }
+ 
+*/
+
+module.exports = (stage, app) => {
+	// For each server (in parallel), run the nginx.js remote script:
+	var serverPromises = stage.workspace.sshServers.map(sshServer => sshServer.server.exec('node', ['/alfred/nginx.js', stage.config.domain], {stream: 'stdout'}));
+	
+	// Await all:
+	return Promise.all(serverPromises).then(() => console.log('NGINX site configured'));
+}
